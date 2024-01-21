d06df9b0f9b239252610815a7388bbe40fe6c38c
bartek1190 <bartlomiej.m.michonski@gmail.com>
2024-01-21 12:42:02 +0100

    finish

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,6 +3,8 @@ import css from './ContactForm.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from '../../redux/operations';
 import { selectContacts } from '../../redux/selectors';
+import { Button, TextField } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
 
 export const ContactForm = () => {
   const dispatch = useDispatch();
@@ -14,56 +16,49 @@ export const ContactForm = () => {
     const newName = form.elements.name.value;
     const newNumber = form.elements.number.value;
 
-    const isNameAlreadyExists = contacts.find(
-      contact => contact.name === newName
-    );
-    const isNumberAlreadyExists = contacts.filter(contact =>
-      contact.phone.includes(newNumber)
-    ).length;
-
-    if (isNameAlreadyExists) {
+    if (contacts.find(contact => contact.name === newName)) {
       alert(`${newName} is already in your contact list.`);
-    } else if (isNumberAlreadyExists) {
+    } else if (
+      contacts.filter(contact => contact.number.includes(newNumber)).length
+    ) {
       alert(`${newName} cannot have the same number as your other contact.`);
     } else if (newName !== '' || newNumber !== '') {
       dispatch(
         addContact({
           name: newName,
-          phone: newNumber,
+          number: newNumber,
         })
       );
       form.reset();
     }
   };
-
-  const generateUniqueId = () => uuidv4();
-  const nameInputId = generateUniqueId();
-  const numberInputId = generateUniqueId();
-
   return (
     <form className={css.contactForm} onSubmit={handleSubmit}>
-      <label htmlFor={nameInputId}>Name</label>
-      <input
+      <TextField
+        size="small"
         className={css.contactForm__input}
+        label="Name"
         type="text"
         name="name"
         pattern="^[a-zA-Zа-яА-Я]+([ -'][a-zA-Zа-яА-Я]+)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        id={nameInputId}
+        id={uuidv4()}
         required
       />
-      <label htmlFor={numberInputId}>Number </label>
-      <input
+      <TextField
+        size="small"
         className={css.contactForm__input}
+        label="Number"
         type="tel"
         name="number"
         pattern="^[+]?[0-9 \u0028\u0029\u002D]*$"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        id={numberInputId}
+        id={uuidv4()}
         required
       />
-
-      <button type="submit">Add contact</button>
+      <Button variant="outlined" type="submit">
+        <PersonIcon /> Add contact
+      </Button>
     </form>
   );
 };
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import css from './Filter.module.css';
 import { selectStatusFilter } from '../../redux/selectors';
 import { setStatusFilter } from '../../redux/filtersSlice';
+import { TextField } from '@mui/material';
+import { v4 as uuidv4 } from 'uuid';
 
 export const Filter = () => {
   const filter = useSelector(selectStatusFilter);
@@ -14,18 +16,19 @@ export const Filter = () => {
 
   return (
     <div className={css.filterDiv}>
-      <label className={css.filterDiv__label}>
-        Find contacts by name
-        <input
-          className={css.filterDiv__input}
-          type="text"
-          name="filter"
-          value={filter}
-          pattern="^[a-zA-Zа-яА-Я]+([ -'][a-zA-Zа-яА-Я]+)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          onChange={handleFilterChange}
-        />
-      </label>
+      <TextField
+        size="small"
+        className={css.filterDiv__input}
+        label="Find contacts by name"
+        type="text"
+        name="filter"
+        defaultValue={filter}
+        pattern="^[a-zA-Zа-яА-Я]+([ -'][a-zA-Zа-яА-Я]+)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        id={uuidv4()}
+        onChange={handleFilterChange}
+        variant="filled"
+      />
     </div>
   );
 };
